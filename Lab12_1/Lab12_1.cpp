24ce449dc2c1bd0ec24c6d849a3d3bc2f40abd65
Tylko-jedno <diogkek@gmail.com>
2024-11-26 12:15:37 +0200

    first master commit

---
+++ Lab12_1/Lab12_1.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cctype>
+#include <Windows.h> 
+
+using namespace std;
+
+void createSourceFile(const char* filename) {
+    fstream file(filename, ios::binary | ios::out);
+    if (!file) {
+        cerr << "Помилка створення файлу: " << filename << endl;
+        return;
+    }
+
+    char data[] = { 'A', '5', 'B', '1', 'C', '9', 'D', 'E', '0', 'F' };
+    file.write(data, sizeof(data));
+    file.close();
+}
+
+void filterFile(const char* source, const char* destination) {
+    fstream inFile(source, ios::binary | ios::in );
+    if (!inFile) {
+        cerr << "Помилка відкриття файлу " << source << endl;
+        return;
+    }
+
+    fstream outFile(destination, ios::binary | ios::out);
+    if (!outFile) {
+        cerr << "Помилка створення файлу " << destination << endl;
+        inFile.close();
+        return;
+    }
+
+    char ch;
+    while (inFile.read(&ch, sizeof(ch))) {
+        if (!isdigit(ch)) {
+            outFile.write(&ch, sizeof(ch));
+        }
+    }
+
+    inFile.close();
+    outFile.close();
+}
+
+void displayFileContent(const char* filename) {
+    fstream file(filename, ios::binary | ios::in);
+    if (!file) {
+        cerr << "Помилка відкриття файлу " << filename << endl;
+        return;
+    }
+
+    char ch;
+    cout << "Вміст файлу " << filename << ": ";
+    while (file.read(&ch, sizeof(ch))) {
+        cout << ch << ' ';
+    }
+    cout << endl;
+
+    file.close();
+}
+
+int main() {
+    const char* sourceFile = "source.dat";
+    const char* destinationFile = "result.dat";
+
+    createSourceFile(sourceFile);               
+    filterFile(sourceFile, destinationFile);    
+    displayFileContent(destinationFile);     
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h" // Підключається автоматично у Visual Studio для модульних тестів
+#include "CppUnitTest.h"
+#include <fstream>
+#include <string>
+#include "../Lab12_1/Lab12_1.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+    TEST_CLASS(UnitTest)
+    {
+    public:
+        TEST_METHOD(TestFilterFile)
+        {
+            const char* sourceFile = "test_source.dat";
+            const char* resultFile = "test_result.dat";
+
+            createSourceFile(sourceFile);
+
+            filterFile(sourceFile, resultFile);
+
+            std::ifstream file(resultFile, std::ios::binary);
+            Assert::IsTrue(file.good(), L"Результуючий файл не створений.");
+
+            char expectedData[] = { 'A', 'B', 'C', 'D', 'E', 'F' };
+            char buffer[sizeof(expectedData)] = {};
+            file.read(buffer, sizeof(buffer));
+            Assert::AreEqual(0, memcmp(buffer, expectedData, sizeof(expectedData)), L"Вміст результуючого файлу не відповідає очікуваному.");
+            file.close();
+        }
+    };
+}
